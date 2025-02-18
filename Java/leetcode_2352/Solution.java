b423c0740606cbb0ae7c731640006f65af8dcb2c
ChristineGoGo <brandywang22@gmail.com>
2025-02-18 05:11:37 -0800

    brute force solution to leetcode 2352

--- Java/leetcode_2352/Solution.java
+++ Java/leetcode_2352/Solution.java
@@ -1,6 +1,4 @@
 
-import java.util.HashMap;
-import java.util.Map;
 
 
 public class Solution {
@@ -18,18 +16,20 @@ public class Solution {
         // Approach : store the columns in a map loop through the rows
         // collecting the count of equal rows and columns
 
-        Map<Integer, int[]> columnStores = new HashMap<>();
-        int n = grid[1].length;
-        int[] current = new int[n];
+        int n = grid.length, count = 0;
 
-        for (int i = 0; i < grid.length; i ++) {
-            for (int j = 0; j < n; j++) {
-                System.out.println(j);
+        for (int r = 0; r < n; r++) {
+            boolean match = true;
+            for (int c = 0; c < n; c++) {
+                for (int i = 0; i < n; i++) {
+                    if (grid[r][i] != grid[i][c]) {
+                        match = false;
+                    }
+                }
+                count += match == true ? 1 : 0;
             }
-            columnStores.put(i, current);
         }
-        System.out.println(columnStores);
-        return 0;
+        return count;
      }
 
      public static void main(String[] args) {
