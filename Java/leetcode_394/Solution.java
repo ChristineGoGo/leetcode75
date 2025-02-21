0114392436ffa1d2457578dec57aa69571cdffab
ChristineGoGo <brandywang22@gmail.com>
2025-02-21 13:55:03 -0800

    leetcode 394

--- Java/leetcode_394/Solution.java
+++ Java/leetcode_394/Solution.java
@@ -1,20 +1,40 @@
 package leetcode_394;
+import java.util.*;
 
 public class Solution {
     public static String decodeString(String s) {
-        Stack<String> stck = new Stack<>();
+        Stack<Integer> count = new Stack<>();
+        Stack<StringBuilder> currentStringStack = new Stack<>();
+        StringBuilder currentString = new StringBuilder();
+        
+        int occurences = 0;
+
         for (int i = 0; i < s.length(); i++) {
-            System.out.println(s.charAt(i));
-            if (s.charAt(i) != ']') {
-                stck.push(s.charAt(i));
+            if (Character.isDigit(s.charAt(i))) {
+                occurences = occurences * 10 + s.charAt(i) - '0';
+            } else if (s.charAt(i) == ('[')) {
+                count.push(occurences);
+                currentStringStack.push(currentString);
+                currentString = new StringBuilder();
+                occurences = 0;
+
+            } else if (s.charAt(i) == ']') {
+                StringBuilder decodedString = currentStringStack.pop();
+                for (int currentCount = count.pop(); currentCount > 0; currentCount--) {
+                    decodedString.append(currentString);
+                }
+                currentString = decodedString;
+            } else {
+                currentString.append(s.charAt(i));
             }
         }
-        return "";
+        return currentString.toString();
     }
 
     public static void main(String[] args) {
-        String str = "3[a2[c]]";
-        System.out.println(decodeString(str));
+        String test1 = "3[a]2[bc]";
+        System.out.println(decodeString(test1));
+
     }
 
 }
