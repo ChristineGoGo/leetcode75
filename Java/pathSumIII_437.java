363b339a7ae09f702b98cb117afa46669b8f24bd
ChristineGoGo <brandywang22@gmail.com>
2025-02-27 06:06:11 -0800

    leetcode 437

--- Java/pathSumIII_437.java
+++ Java/pathSumIII_437.java
@@ -1,6 +1,11 @@
 import java.util.LinkedList;
 import java.util.List;
-
+/*
+ * Two Solutions!Given the root of a binary tree and an integer targetSum, 
+ * return the number of paths where the sum of the values along the path equals targetSum.
+ * The path does not need to start or end at the root or a leaf, but it must go downwards 
+ * (i.e., traveling only from parent nodes to child nodes).
+ */
 public class pathSumIII_437 {
     int counter = 0;
     int target;
@@ -38,3 +43,44 @@ public class pathSumIII_437 {
     }
 
 }
+
+// public class pathSumIII_437 {
+// int counter = 0;
+// int target;
+
+// Map<Long, Integer> countMap = new HashMap<>();
+
+// public int pathSum(TreeNode root, int targetSum) {
+//     target = targetSum;
+
+//     dfs(root, 0L);
+//     return counter;
+    
+// }
+
+// public void dfs(TreeNode node, long currentSum) {
+//     if (node != null) {
+//         // update the curent sum value
+//         currentSum += node.val;
+//         //update the counter
+//         //---------path with target sum starts from the root
+//         if (currentSum == target) {
+//             counter += 1;
+//         }
+//         //--- path with target sum starts else where
+//         counter += countMap.getOrDefault(currentSum - target, 0);
+
+//         // update the hashmap
+//         countMap.put(currentSum, countMap.getOrDefault(currentSum, 0) + 1);
+
+//         // traverse the left side
+//         dfs(node.left, currentSum);
+//         // traverse the right side
+//         dfs(node.right, currentSum);
+        
+//         //remove the last node from the map while backtracking
+//         countMap.put(currentSum, countMap.get(currentSum) - 1);
+//     } else {
+//         return;
+//     }
+// }
