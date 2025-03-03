712c4d998b614fb50affceb443d732299871be6d
ChristineGoGo <brandywang22@gmail.com>
2025-03-03 08:22:04 -0800

    removed extra spaces

--- Java/searchInABinaryTree_700.java
+++ Java/searchInABinaryTree_700.java
@@ -1,9 +1,9 @@
 public class searchInABinaryTree_700 {
     /**
      * You are given the root of a binary search tree (BST) and an integer val.
-     * Find the node in the BST that the node's value equals val and return the 
+     * Find the node in the BST that the node's value equals val and return the
      * subtree rooted with that node. If such a node does not exist, return null.
-     * 
+     *
      * APPROACH: Traverse through the Breadth First Search Iteration
      * Compare each node to the given value's node and return if they are equal
      * OR Use DFS to search using recursion
